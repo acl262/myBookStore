8fb09cf37609bdc5f24179b8e2d5fe52fd1fea4b
Chris <acl8923@gmail.com>
2018-06-01 21:33:14 -0400

    Initial commit

---
+++ src/main/java/com/bookstore/BookstoreNewApplication.java
@@ -0,0 +1,12 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreNewApplication.class, args);
+	}
+}
