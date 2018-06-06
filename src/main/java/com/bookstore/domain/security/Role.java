292baeb4de1ba3ad71fb4b38b26dc95de996b137
Chris <acl8923@gmail.com>
2018-06-05 20:16:33 -0400

    add security and login

---
+++ src/main/java/com/bookstore/domain/security/Role.java
@@ -0,0 +1,46 @@
+package com.bookstore.domain.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Role {
+
+    @Id
+    private int roleId;
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    public int getRoleId() {
+        return this.roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return this.userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+}
